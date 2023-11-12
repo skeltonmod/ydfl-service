af2e062b0a38e3fc3306c5c6588d269d7015979b
novidash <abgaoe@gmail.com>
2023-11-12 17:48:58 +0800

    revert

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,6 @@ var router = express.Router();
 const ytdl = require("ytdl-core");
 const ffmpeg = require("fluent-ffmpeg");
 const pathToFfmpeg = require("ffmpeg-static");
-const path = require("path");
-const fs = require("fs");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -14,49 +12,20 @@ router.get("/", function (req, res, next) {
 router.get("/fetch-video/:id", async function (req, res, next) {
   const id = req.params.id;
   const url = `https://www.youtube.com/watch?v=${id}`;
-
   const audio = ytdl(url, {
     filter: "audioonly",
     quality: "highestaudio",
   });
   res.setHeader("Content-type", "audio/mpeg");
-  const randomString = [...Array(12)]
-    .map(() =>
-      String.fromCharCode(
-        Math.floor(Math.random() * 26) + (Math.random() < 0.5 ? 65 : 97)
-      )
-    )
-    .join("");
-
-  const outputFilePath = path.join(__dirname, `${randomString}.mp3`);
-  const outputStream = fs.createWriteStream(outputFilePath);
-
-  ffmpeg(audio)
-    .audioCodec("libmp3lame")
-    .audioBitrate(128)
-    .format("mp3")
-    .on("error", (err) => console.error(err))
-    .on("end", () => {
-      console.log("Finished!");
-      res.sendFile(outputFilePath, {}, (err) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send("Error sending file");
-        } else {
-          // Delete the file after it has been sent
-          fs.unlink(outputFilePath, (unlinkErr) => {
-            if (unlinkErr) {
-              console.error(unlinkErr);
-            } else {
-              console.log("File deleted");
-            }
-          });
-        }
-      });
-    })
-    .pipe(outputStream, {
-      end: true,
-    });
+  var proc = new ffmpeg({ source: audio });
+  proc.setFfmpegPath(pathToFfmpeg);
+  proc.withAudioCodec("libmp3lame").toFormat("mp3").output(res).run();
+  proc.on("error", function (e) {
+    console.log("ooopsie!", e);
+  });
+  proc.on("end", function () {
+    console.log("finished");
+  });
 });
 
 module.exports = router;
